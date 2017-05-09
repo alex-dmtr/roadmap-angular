1ae0524095d15638435c392923682b03e2cbee56
Alex Dumitru <alexadrian.dmtr@gmail.com>
2017-05-09 11:01:40 +0300

    Stub groups

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,31 @@
 import { Component } from '@angular/core';
 
+class User {
+  id?: Number
+  username: String
+}
+class Group {
+  id?: Number
+  name: String
+  description: String
+  avatarUrl: String
+  owner: User
+  members?: User[]
+}
 @Component({
   selector: 'app',
   templateUrl: './app.template.html'
 })
-export class AppComponent {
 
+export class AppComponent {
+  Groups: Group[] = [
+    {
+      "name": "Explorers",
+      "description": "Hiking and outdoor activities",
+      "avatarUrl": "http://s.hswstatic.com/gif/how-to-hike-1.jpg",
+      owner: {
+        username: "jango.fett"
+      }
+    }
+  ];
 }
