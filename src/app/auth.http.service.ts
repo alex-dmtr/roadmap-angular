7f21b6baa0726cc2b0403753fa8dcba14ab7adb2
Alex Dumitru <alexadrian.dmtr@gmail.com>
2017-05-11 11:12:09 +0300

    Create AuthHttpService
    For making JWT-authenticated http requests

---
+++ src/app/auth.http.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { AuthService } from './auth.service';
+import { Headers, Http, Response, ResponseOptionsArgs } from '@angular/http';
+
+import { Observable } from 'rxjs/Observable';
+@Injectable()
+export class AuthHttpService {
+  constructor(private http: Http, private authService: AuthService) {
+
+  }
+
+  get(url: string, options?: ResponseOptionsArgs): Observable<Response> {
+    let _options: ResponseOptionsArgs = options || {};
+
+    _options.headers = _options.headers || new Headers();
+
+    _options.headers.append('Authorization', `Bearer ${this.authService.getToken()}`);
+
+    return this.http.get(url, _options);
+  }
+}
