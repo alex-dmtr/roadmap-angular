a8c041338a474968bb0c2a0307069878b72d897a
Alex Dumitru <alexadrian.dmtr@gmail.com>
2017-05-17 12:55:49 +0300

    Redirect 'home' to groups on auth

--- src/app/home.component.ts
+++ src/app/home.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-home',
-  template: `  <h2>Home</h2>
-  <p>Welcome to our groups website! You can meet awesome people here.</p>
-  <p>Joining is easy. Just click <a routerLink='/register'>here</a> to go to to the sign up page.</p>`
+  templateUrl: './home.template.html'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  constructor(
+    private router: Router,
+    private authService: AuthService
+  ) {
 
+  }
+
+  ngOnInit() {
+    if (this.authService.isAuthenticated())
+      this.router.navigate(["groups"]);
+  }
 }
